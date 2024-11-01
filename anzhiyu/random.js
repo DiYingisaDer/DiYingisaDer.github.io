f9ace7a2dd1fc105bf2c185ca764489b92468910
DiYingisaDer <diyingisader@outlook.com>
2024-11-01 16:15:08 +0800

    Site updated: 2024-11-01 16:15:02

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["2024/10/21/Markdown-Grammar-2/","2024/10/20/Markdown-Grammar-1/","2024/10/22/WangWenXieZuo-1/","2024/10/23/WangWenXieZuo-2/","2024/10/24/WangWenXieZuo-3/","2024/10/19/hello-world/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
